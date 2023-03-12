251bbd0287649b1a45ca2d0145717974888a684f
TrinhNC <nguyencongtrinh.vn@gmail.com>
2023-03-12 18:24:42 +0100

    Update sign_to_controller script

--- src/sign_to_controller.py
+++ src/sign_to_controller.py
@@ -8,39 +8,42 @@ import cv2 as cv
 class GestureController:
 
     def __init__(self):
-        rospy.init_node('sign_to_controller', anonymous=True)
-
+        rospy.init_node('hand_sign_control', anonymous=True)
+        # Subscriber for subscribing the hand signs
         self.gesture_subcriber = rospy.Subscriber(rospy.get_param("hand_sign_recognition/publish_gesture_topic"), 
                                                 String, self.callback)
-        # Publisher which will publish to the topic 
-        self.vel_publisher = rospy.Publisher("/mobile_robot/cmd_vel", Twist, queue_size=10)
+        # Publisher for publishing velocities 
+        self.vel_publisher = rospy.Publisher("/robot_diff_drive_controller/cmd_vel", Twist, queue_size=10)
+        # Velocity message
         self.vel_msg = Twist()
+        # Velocity increments
+        self.linear_vel = 0.01 #[m/s]
+        self.angular_vel = 0.1 #[rad/s]
 
     def callback(self, gesture):
-        print(gesture.data)
+        """Convert the current hand sign to a velocity command.
+        The command is put to self.vel_msg which is published to topic "/robot_diff_drive_controller/cmd_vel".
+
+        Args:
+            gesture (string): Forward, Backward, Turn Right, Turn Left, Stop, NONE
+            Linear velocity is m/s. Angular velocity is rad/s.
+        """
         if gesture.data ==  "Forward":
-            self.vel_msg.linear.x += 0.01
+            self.vel_msg.linear.x += self.linear_vel
+            self.vel_msg.angular.z = 0.
         elif gesture.data ==  "Backward":
-            self.vel_msg.linear.x -= 0.01
+            self.vel_msg.linear.x -= self.linear_vel
+            self.vel_msg.angular.z = 0.
         elif gesture.data == "Turn Right":
-            self.vel_msg.angular.z += 0.1
+            self.vel_msg.angular.z -= self.angular_vel
         elif gesture.data == "Turn Left":
-            self.vel_msg.angular.z -= 0.1
-        elif gesture.data == "Stop":
+            self.vel_msg.angular.z += self.angular_vel
+        elif gesture.data == "Stop" or gesture.data == "NONE" :
             self.vel_msg.linear.x = 0
             self.vel_msg.angular.z = 0
-
-        if self.vel_msg.linear.x >= 3. :
-            self.vel_msg.linear.x = 3.
-        if self.vel_msg.angular.z >= 3. :
-            self.vel_msg.angular.z = 3
-
-        # self.vel_msg.linear.y = 0
-        # self.vel_msg.linear.z = 0
-        # self.vel_msg.angular.x = 0
-        # self.vel_msg.angular.y = 0
         
         self.vel_publisher.publish(self.vel_msg)
+        print(gesture.data, " Linear: {0} m/s, Angular: {1} rad/s".format(round(self.vel_msg.linear.x,6), round(self.vel_msg.angular.z,6)))
         
 
 if __name__=="__main__":
